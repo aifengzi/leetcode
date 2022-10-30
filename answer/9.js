1a27f15048da5060dde0e8931c2645ce654453a9
fengzi <fengzi880810@163.com>
2022-10-30 23:42:44 +0800

    添加"回文数"问题的答案

--- answer/9.js
+++ answer/9.js
@@ -2,3 +2,13 @@
  * 本文件记录leetcode官网题库中'回文数'题目的答案（仅属于自己联系和交流）。
  * leetcode网站: https://leetcode-cn.com/
  */
+
+/**
+ * @param {number} x 需要检查的数字
+ * @return {boolean} 是否回文数字
+ */
+const isPalindrome = (x) => {
+  const str = `${x}`;
+
+  return str === str.split('').reverse().join('');
+};
