3e1babe11713127b3cd925030580aa36fe3b2224
fengzi <fengzi880810@163.com>
2022-11-14 22:04:03 +0800

    feat(files): 添加答案文件

---
+++ answer/20.js
@@ -0,0 +1,16 @@
+/**
+ * 本文件记录leetcode官网题库中'回文数'题目的答案（仅属于自己联系和交流）。
+ * leetcode网站: https://leetcode-cn.com/
+ */
+
+/**
+ * @param {string} str 需要检查的字符串
+ * @return {boolean} 是否符合规范
+ */
+const isValid = (str) => {
+  
+};
+
+// test
+console.log(isValid());
+console.log(isValid());
